9c21167421f847686497ed53f1df5c2af4e30eb0
Fabrice Nicol <fabrnicol@gmail.com>
2021-04-13 09:54:09 +0200

    Add support for Integer and double (float) vectors.

--- test0.m
+++ test0.m
@@ -22,6 +22,9 @@
 
 :- import_module io.
 
+:- type float_buffer.
+:- type int_buffer.
+
 :- impure pred main(io::di, io::uo) is det.
 
 %----------------------------------------------%
@@ -42,7 +45,7 @@
 
 :- pragma foreign_decl("C", "#include <Rinternals.h>").
 
-:- pragma foreign_code("C", "int start = 1;").
+:- pragma foreign_decl("C", "int start = 1;").
 
 :- impure pred start_r is det.
 
@@ -56,8 +59,8 @@
 
 :- pred stop_r is det.
 
-    % Call this predicate at the end of a session.  
-    % For ill-understood reasons, this RInside function (01.21) 
+    % Call this predicate at the end of a session.
+    % For ill-understood reasons, this RInside function (01.21)
     % does not reset setup status to zero, so start_r
     % should not be called as would be expected
 
@@ -65,6 +68,10 @@
                        stop_r,
                        [promise_pure, will_not_call_mercury],
                        "teardownRinC();").
+
+%-----------------------------------------------------------------------------%
+% Sourcing R code
+%
 
 :- impure pred source(string::in) is det.
 
@@ -94,7 +101,7 @@
     % Predicates implementated as semipure (promised pure
     % or hopefully so). Starting with one-dimensional
     % real or integer output.
-    % Predicates of arity one flagged impure 
+    % Predicates of arity one flagged impure
     % pending further evaluation.
 
 %% Mercury type           C type
@@ -111,6 +118,10 @@
 %% float                  MR_Float
 %% char                   MR_Char
 %% string                 MR_String
+
+%-----------------------------------------------------------------------------%
+% Basic types
+%
 
 :- typeclass r_eval(T) where [
        pred eval(string::in, T::out) is det
@@ -132,10 +143,10 @@
        pred(eval/2) is eval_float
    ].
 
-    % Predicates instanciating r_eval may be promised pure, 
-    % but do not rely on them for invoking internal state 
+    % Predicates instanciating r_eval may be promised pure,
+    % but do not rely on them for invoking internal state
     % variables across calls.  Output is crushed.
-    % R variables are threaded to Mercury 
+    % R variables are threaded to Mercury
     % using the output mode variables.
 
 :- pred eval_string(string::in, string::out) is det.
@@ -197,13 +208,124 @@ if (inherits(z, 'try-error')) E <- 'R string error' else E <- z"");
                        "
                          if (start) setupRinC();
                          start = 0;
-                         MR_Bool a = evalInRToBool(X) ? 
+                         MR_Bool a = evalInRToBool(X) ?
                                        MR_YES : MR_NO ;
                          Z = (MR_Bool) a;
                        ").
 
-% Testbed: playing with R I/O and retrieving R computations 
+% ---------------------------------------------------------------------------%
+% Vectors
+%
+
+% Note that C-structures shoud remain flat otherwise the GC will lose track.
+% A first-rank Integer field is OK.
+
+:- pragma foreign_decl("C", "
+
+#include ""mercury_float.h""    /* For MR_FLT_FMT. */
+#include ""mercury_memory.h""
+#include ""mercury_string.h""
+
+#include <stdio.h>  /* For sscanf. */
+
+typedef struct {
+    MR_Integer  size;
+    MR_Float   *contents;
+} FLOAT_BUFFER;
+
+typedef struct {
+    MR_Integer  size;
+    MR_Integer  *contents;
+} INT_BUFFER;
+
+").
+
+    % Mercury types corresponding to integer() and numeric()
+    % with respective types vector(integer) and vector(double)
+
+:- pragma foreign_type("C", int_buffer, "INT_BUFFER *",
+                       [can_pass_as_mercury_type]).
+
+:- pragma foreign_type("C", float_buffer, "FLOAT_BUFFER *",
+                       [can_pass_as_mercury_type]).
+
+    % float_vect(R_Code, float_buffer)).
+    % Parse R code into a Mercury float buffer.
+
+:- pred float_vect(string::in, float_buffer::out) is det.
+
+
+:- pragma foreign_proc("C",
+                       float_vect(X::in, Buffer::out),
+                       [will_not_call_mercury, promise_pure],
+                       "
+                         if (start) setupRinC();
+                         start = 0;
+                         SEXP V = evalInR(X);
+                         MR_Float *Items = (MR_Float *) REAL(V);
+                         Buffer = MR_GC_NEW(FLOAT_BUFFER);
+                         Buffer->size = LENGTH(V);
+                         Buffer->contents = Items;
+                       ").
+
+:- pred int_vect(string::in, int_buffer::out) is det.
+
+:- pragma foreign_proc("C",
+                       int_vect(X::in, Buffer::out),
+                       [will_not_call_mercury, promise_pure],
+                       "
+                         if (start) setupRinC();
+                         start = 0;
+                         SEXP V = evalInR(X);
+                         Buffer = MR_GC_NEW(INT_BUFFER);
+                         MR_Integer S = LENGTH(V);
+                         MR_Float *V1 = REAL(V);
+                         Buffer->size = S;
+                         Buffer->contents = MR_GC_malloc(sizeof(MR_Integer) * S);
+                         for (int i = 0; i < S; ++i)
+                            Buffer->contents[i] = floor(V1[i]);
+                       ").
+
+:- pred lookup_int(int::in, int_buffer::in, int::out) is det.
+:- pred lookup_int_size(int_buffer::in, int::out) is det.
+:- pred lookup_float_size(float_buffer::in, int::out) is det.
+:- pred lookup_float(int::in, float_buffer::in, float::out) is det.
+
+:- pragma foreign_proc("C",
+                       lookup_int_size(Buffer::in, Value::out),
+                       [will_not_call_mercury, promise_pure],
+                       "
+                             Value=(MR_Integer) Buffer->size;
+                       ").
+
+:- pragma foreign_proc("C",
+                       lookup_float_size(Buffer::in, Value::out),
+                       [will_not_call_mercury, promise_pure],
+                       "
+                             Value=(MR_Integer) Buffer->size;
+                       ").
+
+:- pragma foreign_proc("C",
+                       lookup_float(Index::in, Buffer::in, Value::out),
+                       [will_not_call_mercury, promise_pure],
+                       "
+                             Value=(MR_Float) Buffer->contents[Index];
+                       ").
+
+:- pragma foreign_proc("C",
+                       lookup_int(Index::in, Buffer::in, Value::out),
+                       [will_not_call_mercury, promise_pure],
+                       "
+                             Value=(MR_Integer) Buffer->contents[Index];
+                       ").
+
+% It should be easy to convert int_buffer or float_buffer into arrays or lists.
+
+%----------------------------------------------------------------------------%
+% Testbed: playing with R I/O and retrieving R computations
 % in Mercury variables through the C foreign code interface.
+%
+
 
 main(!IO) :-
              impure source("
@@ -236,6 +358,16 @@ main(!IO) :-
              io.write_int(Y2, !IO), io.nl(!IO),
              io.write_float(Z,!IO), io.nl(!IO),
              io.write_int(T,  !IO), io.nl(!IO),
+             float_vect("c(1.0,2.0,3.0,4.0,5.0)", B),
+             int_vect("c(1,2,3,4)", C),
+             lookup_int(2, C, W),
+             lookup_float(3, B, VV),
+             lookup_int_size(C, H),
+             lookup_float_size(B, ZZ),
+             io.write_int(W, !IO), io.nl(!IO),
+             io.write_float(VV, !IO), io.nl(!IO),
+             io.write_int(H, !IO), io.nl(!IO),
+             io.write_int(ZZ, !IO), io.nl(!IO),
              stop_r .
 
 %% Raw R-managed output (can one funnel it through Mercury IO module?):
@@ -265,11 +397,15 @@ main(!IO) :-
 %% 0.8414709568023682
 %% 58
 
-%% %% real	0m0,371s
-%% user	0m0,334s
-%% sys  0m0,037s
+%% Vector lookups:
+%%
+%% 3
+%% 4.0
+%% 4
+%% 5
 
-% TODO: expand this POC to vectorized data structures, 
-% which will request setting precise equivalence relations 
-% between C, R (via the SEXP C-interface) and Mercury data types.
-% This does not look as intractable.
+%% real	0m0,357s
+%% user	0m0,308s
+%% sys  0m0,032s
+
+% TODO: expand this POC to other vectorized data structures.
