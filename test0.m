65c040460f4c19d969244b243f47695814dd9ba2
Fabrice Nicol <fabrnicol@gmail.com>
2021-04-13 17:11:49 +0200

    merge1

--- test0.m
+++ test0.m
@@ -24,6 +24,9 @@
 
 :- impure pred main(io::di, io::uo) is det.
 
+:- type float_buffer.
+:- type int_buffer.
+
 %----------------------------------------------%
 
 :- implementation.
@@ -33,7 +36,10 @@
 :- import_module char.
 :- import_module float.
 :- import_module string.
+:- import_module univ.
 :- import_module math.
+:- import_module list.
+:- import_module array.
 
         % Predicates for sourcing and evaluating R code.  Predicates
         % performing C I/O, semipure or impure (pending evaluation).
@@ -42,7 +48,7 @@
 
 :- pragma foreign_decl("C", "#include <Rinternals.h>").
 
-:- pragma foreign_code("C", "int start = 1;").
+:- pragma foreign_decl("C", "int start = 1;").
 
 :- impure pred start_r is det.
 
@@ -56,8 +62,8 @@
 
 :- pred stop_r is det.
 
-    % Call this predicate at the end of a session.  
-    % For ill-understood reasons, this RInside function (01.21) 
+    % Call this predicate at the end of a session.
+    % For ill-understood reasons, this RInside function (01.21)
     % does not reset setup status to zero, so start_r
     % should not be called as would be expected
 
@@ -94,7 +100,7 @@
     % Predicates implementated as semipure (promised pure
     % or hopefully so). Starting with one-dimensional
     % real or integer output.
-    % Predicates of arity one flagged impure 
+    % Predicates of arity one flagged impure
     % pending further evaluation.
 
 %% Mercury type           C type
@@ -132,10 +138,10 @@
        pred(eval/2) is eval_float
    ].
 
-    % Predicates instanciating r_eval may be promised pure, 
-    % but do not rely on them for invoking internal state 
+    % Predicates instanciating r_eval may be promised pure,
+    % but do not rely on them for invoking internal state
     % variables across calls.  Output is crushed.
-    % R variables are threaded to Mercury 
+    % R variables are threaded to Mercury
     % using the output mode variables.
 
 :- pred eval_string(string::in, string::out) is det.
@@ -165,6 +171,7 @@ if (inherits(z, 'try-error')) E <- 'R string error' else E <- z"");
          Y = a;
     ").
 
+
 :- pred eval_int(string::in, int::out) is det.
 
 :- pragma foreign_proc("C",
@@ -175,6 +182,133 @@ if (inherits(z, 'try-error')) E <- 'R string error' else E <- z"");
                          start = 0;
                          MR_Integer a = evalInRToInt(X);
                          Z = (MR_Integer) a;
+                       ").
+
+
+:- pragma foreign_decl("C", "
+
+#include ""mercury_float.h""    /* For MR_FLT_FMT. */
+#include ""mercury_memory.h""
+#include ""mercury_string.h""
+
+#include <stdio.h>  /* For sscanf. */
+
+typedef struct {
+    MR_Integer  size;
+    MR_Float   *contents;
+} FLOAT_BUFFER;
+
+typedef struct {
+    MR_Integer  size;
+    MR_Integer  *contents;
+} INT_BUFFER;
+
+").
+
+:- pragma foreign_type("C", int_buffer, "INT_BUFFER *",
+                       [can_pass_as_mercury_type]).
+
+:- pragma foreign_type("C", float_buffer, "FLOAT_BUFFER *",
+                       [can_pass_as_mercury_type]).
+
+    % eval_int_vect(R_Code, Array(float)).
+    % Parse R code into a Mercury float array of length Size.
+
+:- pred float_vect(string::in, float_buffer::out) is det.
+
+:- pragma foreign_proc("C",
+                       float_vect(X::in, Buffer::out),
+                       [will_not_call_mercury, promise_pure, thread_safe, will_not_modify_trail,
+                        does_not_affect_liveness],
+                       "
+                         if (start) setupRinC();
+                         start = 0;
+                         SEXP V = evalInR(X);
+                         MR_Float *Items = (MR_Float *) REAL(V);
+                         Buffer = MR_GC_NEW(FLOAT_BUFFER);
+                         Buffer->size = LENGTH(V);
+                         Buffer->contents = Items;
+                       ").
+
+:- pred int_vect(string::in, int_buffer::out) is det.
+
+:- pragma foreign_proc("C",
+                       int_vect(X::in, Buffer::out),
+                       [will_not_call_mercury, promise_pure, thread_safe, will_not_modify_trail,
+                        does_not_affect_liveness],
+                       "
+                         if (start) setupRinC();
+                         start = 0;
+                         SEXP V = evalInR(X);
+                         Buffer = MR_GC_NEW(INT_BUFFER);
+                         MR_Integer S = LENGTH(V);
+                         MR_Float *V1 = REAL(V);
+                         Buffer->size = S;
+                         Buffer->contents = MR_GC_malloc(sizeof(MR_Integer) * S);
+                         for (int i = 0; i < S; ++i) Buffer->contents[i] = floor(V1[i]);
+                       ").
+
+:- pred lookup_int(int::in, int_buffer::in, int::out) is det.
+:- pred lookup_int_size(int_buffer::in, int::out) is det.
+:- pred lookup_float_size(float_buffer::in, int::out) is det.
+:- pred lookup_float(int::in, float_buffer::in, float::out) is det.
+
+:- pragma foreign_proc("C",
+                       lookup_int_size(Buffer::in, Value::out),
+                       [will_not_call_mercury, promise_pure, thread_safe, will_not_modify_trail,
+                        does_not_affect_liveness
+                       ],
+                       "
+                             Value=(MR_Integer) Buffer->size;
+                       ").
+
+:- pragma foreign_proc("C",
+                       lookup_float_size(Buffer::in, Value::out),
+                       [will_not_call_mercury, promise_pure, thread_safe, will_not_modify_trail,
+                        does_not_affect_liveness
+                       ],
+                       "
+                             Value=(MR_Integer) Buffer->size;
+                       ").
+
+:- pragma foreign_proc("C",
+                       lookup_float(Index::in, Buffer::in, Value::out),
+                       [will_not_call_mercury, promise_pure, thread_safe, will_not_modify_trail,
+                        does_not_affect_liveness
+                       ],
+                       "
+                             Value=(MR_Float) Buffer->contents[Index];
+                       ").
+
+:- pragma foreign_proc("C",
+                       lookup_int(Index::in, Buffer::in, Value::out),
+                       [will_not_call_mercury, promise_pure, thread_safe, will_not_modify_trail,
+                        does_not_affect_liveness
+                       ],
+                       "
+                             Value=(MR_Integer) Buffer->contents[Index];
+                       ").
+
+
+
+:- pred eval_int_vect(string::in, int::out, array(int)::array_uo) is det.
+
+:- pragma foreign_proc("C",
+                       eval_int_vect(X::in, S::out, Z::array_uo),
+                       [will_not_call_mercury, promise_pure, thread_safe, will_not_modify_trail,
+                        does_not_affect_liveness
+                       ],
+                       "
+                         if (start) setupRinC();
+                         start = 0;
+                         SEXP V = evalInR(X);
+                         PROTECT(V);
+                         S = (int) LENGTH(V);
+                         MR_Integer *Items = (MR_Integer *) INTEGER(V);
+                         MR_Integer *Items2 = MR_GC_NEW_ARRAY(int,S);
+                         for (int i = 0; i < S; ++i) Items2[i]=Items[i];
+                         Z = Items2;
+                         UNPROTECT(1);
                        ").
 
 :- pred eval_float(string::in, float::out) is det.
@@ -197,45 +331,40 @@ if (inherits(z, 'try-error')) E <- 'R string error' else E <- z"");
                        "
                          if (start) setupRinC();
                          start = 0;
-                         MR_Bool a = evalInRToBool(X) ? 
+                         MR_Bool a = evalInRToBool(X) ?
                                        MR_YES : MR_NO ;
                          Z = (MR_Bool) a;
                        ").
 
-% Testbed: playing with R I/O and retrieving R computations 
+:- pred test_array(list(int)::in, array(int)::out) is det.
+
+test_array(L, A) :- A = array.from_list(L).
+
+
+%-----------------------------------------%
+
+
+% Testbed: playing with R I/O and retrieving R computations
 % in Mercury variables through the C foreign code interface.
 
 main(!IO) :-
-             impure source("
-                 library(data.table)
-                 y <- data.table(c(1,2,3), c(2,3,4))
-                 z <- data.table(c(1,2,3), c(2,3,11))
-             "),
-             impure source_echo("y[V1 == 3]"),
-             impure source_echo("
-                 library(data.table)
-                 fread(file = 'a.csv')
-             "),
-             impure source_echo("y[V1 == 3]"),
-             impure source_echo("z[V1 == 2]"),
-             eval("y <- 12.0; z <- 11.5", X),
-             eval("y <- ""abcd""", WW),
-             impure source_echo("E"),
-             eval_int("z <- 9", Q),
-             impure source_echo("z"),
-             eval_bool("u <- TRUE", U),
-             eval_float("y <- 2.0", V),
-             eval_int("cat('eval z: ', z, '\\n'); z", Y2),
-             eval_float("sin(1.0)", Z),
-             eval_int("8 + 50", T),
-             io.write_string(WW, !IO), io.nl(!IO),
-             io.write_float(X,!IO), io.nl(!IO),
-             io.write_float(V,  !IO), io.nl(!IO),
-             io.write(U,  !IO), io.nl(!IO),
-             io.write_int(Q,  !IO), io.nl(!IO),
-             io.write_int(Y2, !IO), io.nl(!IO),
-             io.write_float(Z,!IO), io.nl(!IO),
-             io.write_int(T,  !IO), io.nl(!IO),
+             test_array([1, 2, 3, 4], A),
+             io.write_int(lookup(A, 0), !IO),io.nl(!IO),
+             float_vect("c(1.0,2.0,3.0,4.0,5.0)", B),
+             copy_to_array(B, Array),
+             io.write_float(array.lookup(Array, 2), !IO), io.nl(!IO),
+             int_vect("c(1,2,3,4)", C),
+%             eval_int_vect("as.integer(c(1,2,3,4))", U, C),
+%             io.write_int(S, !IO), io.nl(!IO),
+             lookup_float(3, B, V),
+             lookup_int(2, C, W),
+             lookup_int_size(C, H),
+             lookup_float_size(B, Z),
+             io.write_int(W, !IO), io.nl(!IO),
+             io.write_float(V, !IO), io.nl(!IO),
+             io.write_int(H, !IO), io.nl(!IO),
+             io.write_int(Z, !IO), io.nl(!IO),
+%             io.write_int(lookup(C,1), !IO), io.nl(!IO),
              stop_r .
 
 %% Raw R-managed output (can one funnel it through Mercury IO module?):
@@ -269,7 +398,7 @@ main(!IO) :-
 %% user	0m0,334s
 %% sys  0m0,037s
 
-% TODO: expand this POC to vectorized data structures, 
-% which will request setting precise equivalence relations 
+% TODO: expand this POC to vectorized data structures,
+% which will request setting precise equivalence relations
 % between C, R (via the SEXP C-interface) and Mercury data types.
 % This does not look as intractable.
